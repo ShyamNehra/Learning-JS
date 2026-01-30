49bdb0999daba4736fb7114c02dc95a029c0b8b6
Shyam Nehra <nehrashyam05@gmail.com>
2026-01-31 00:40:04 +0530

    Global and Block scope

---
+++ 9-Scope.js
@@ -0,0 +1,33 @@
+// let, const are block scoped while var is global scoped   
+
+let a = 300;
+var b = 500;
+const c = 333;
+{
+    let a = 999
+    var b = 432
+    const c = 908
+    console.log("Inner a: ", a)
+    console.log("Inner b: ", b)
+    console.log("Inner c: ", c)
+}
+console.log("Outer a: ", a)
+console.log("Outer b: ", b)
+console.log("Outer c: ", c)
+
+//In case of nested function, child function can use values from parents functions
+
+function one(){
+    const yourname = "Narendra";
+    
+    function two(){
+        const yourprivatename = "Nandu"
+        console.log(yourname)
+        console.log(yourprivatename)
+    }
+
+   //console.log(yourprivatename) // it can't be accessed outside 'two'
+   two();
+}
+
+one();
