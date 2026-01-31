8d927ceba2085a5a00a71a59c5663f6c3d691271
Shyam Nehra <nehrashyam05@gmail.com>
2026-01-31 12:38:20 +0530

    Scope

--- 9-Scope.js
+++ 9-Scope.js
@@ -1,33 +1,45 @@
-// let, const are block scoped while var is global scoped   
+// let, const are block scoped while var is global scoped
 
 let a = 300;
 var b = 500;
 const c = 333;
 {
-    let a = 999
-    var b = 432
-    const c = 908
-    console.log("Inner a: ", a)
-    console.log("Inner b: ", b)
-    console.log("Inner c: ", c)
+  let a = 999;
+  var b = 432;
+  const c = 908;
+  console.log("Inner a: ", a);
+  console.log("Inner b: ", b);
+  console.log("Inner c: ", c);
 }
-console.log("Outer a: ", a)
-console.log("Outer b: ", b)
-console.log("Outer c: ", c)
+console.log("Outer a: ", a);
+console.log("Outer b: ", b);
+console.log("Outer c: ", c, "\n");
 
-//In case of nested function, child function can use values from parents functions
+//In case of nested function, child function can use values from parent's functions, but can't access values outside a block
 
-function one(){
-    const yourname = "Narendra";
-    
-    function two(){
-        const yourprivatename = "Nandu"
-        console.log(yourname)
-        console.log(yourprivatename)
-    }
+function one() {
+  const yourname = "Narendra";
 
-   //console.log(yourprivatename) // it can't be accessed outside 'two'
-   two();
+  function two() {
+    const yourprivatename = "Nandu";
+    console.log(yourname);
+    console.log(yourprivatename, "\n");
+  }
+
+  //console.log(yourprivatename) // it can't be accessed outside 'two'
+  two();
 }
 
 one();
+
+
+//another example
+if (true) {
+    const username = "Shyam"
+    if(username === "Shyam"){
+        const message = "Welcome!"
+        console.log(message + username)
+    }
+    // console.log(message) - can't be accessed outside block
+}
+// console.log(username) - can't be accessed outside block
